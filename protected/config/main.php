e67f5abb05ec3dd871854cc36ed7ef2574352b1b
root <root@nas.in-doors.ru>
2012-10-03 18:06:30 +0400

    CRUD for table tbl_user

--- protected/config/main.php
+++ protected/config/main.php
@@ -19,15 +19,12 @@ return array(
 	),
 
 	'modules'=>array(
-		// uncomment the following to enable the Gii tool
-		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'h632&G3h8c',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
+			'ipFilters'=>array('127.0.0.1','::1','10.105.12.217'),
 		),
-		*/
 	),
 
 	// application components
@@ -46,11 +43,6 @@ return array(
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-    /*
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
-    */
 		'db'=>array(
 			'connectionString' => 'mysql:host=localhost;dbname=controlpanel',
 			'emulatePrepare' => true,
