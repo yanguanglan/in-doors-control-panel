15093e692d75fe0edcf5843b277e60ec1d2ffd4d
Dmitry Nezhelskoy <dmitry@nezhelskoy.ru>
2013-11-27 21:41:01 +0400

    SQL queries from controller moved to model. Code not tested

--- protected/modules/billing/models/UTM5PaymentTransactions.php
+++ protected/modules/billing/models/UTM5PaymentTransactions.php
@@ -57,6 +57,63 @@ class UTM5PaymentTransactions extends UTM5ActiveRecord
     );
   }
   
+  public function getAllPaymentsSqlDataProvider($userId)
+  {
+    $user = self::model()->findByPk($userId);
+    if($user === null) {
+      throw new CHttpException(404, 'The requested user does not exist.');
+    }
+
+    // Полубение информации об архивных таблицах
+    $table_names = Yii::app()->dbUTM5->createCommand('SELECT * FROM `archives` WHERE `table_type` = 7 ORDER BY `end_date` DESC')->queryAll();
+
+    $main_sql = '
+SELECT
+  `t`.`id` AS id,
+  `m`.`name` AS method,
+  `t`.`payment_absolute` AS pay,
+  `t`.`payment_incurrency` AS pay_curr,
+  `c`.`currency_brief_name` AS curr,
+  `t`.`actual_date` AS actual_date,
+  `t`.`payment_enter_date` AS enter_date,
+  `t`.`comments_for_user` AS user_comment,
+  `t`.`comments_for_admins` AS admin_comment,
+  `u`.`login` AS who_receive,
+  `t`.`burn_time` AS burn_time,
+  `t`.`is_canceled` AS canceled
+FROM
+  `payment_transactions` `t`
+  INNER JOIN `accounts` `a` ON `t`.`account_id` = `a`.`id`
+  LEFT JOIN `payment_methods` `m` ON `t`.`method` = `m`.`id`
+  LEFT JOIN `currency_list` `c` ON `t`.`currency_id` = `c`.`id`
+  LEFT JOIN (SELECT `id`, `login` FROM `system_accounts`
+             UNION
+             SELECT `id`, `login` FROM `users`) `u` ON `t`.`who_receive` = `u`.`id`
+WHERE
+  `t`.`account_id` = ' . $user->basic_account . '
+';
+    $sqls = array();
+    $sqls[] = $main_sql;
+    foreach ($table_names as $table_name) {
+      $sqls[] = str_replace('`payment_transactions`', $table_name['table_name'], $main_sql);
+    }
+    $union = '
+UNION
+';
+    $sql = implode($union, $sqls);
+    
+    if (Yii::app()->user->name !== "admin") {
+      $sql .= ' LIMIT 1';
+    }
+
+    //Yii::trace(CVarDumper::dumpAsString($sql));
+    $dataProvider = new CSqlDataProvider($sql, array('pagination'=>false,));
+    $dataProvider->db = Yii::app()->dbUTM5;
+    
+    return $dataProvider;
+
+  }
+  
 }
 
 ?>
